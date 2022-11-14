b2c77efc4c9ace67857b1fc151c7cae901e30534
kevinkkimutai <kiruikevinkim@gmail.com>
2022-11-14 18:51:12 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+    }
+    function sayHiToHeadphonedRoommate(string) {
+        var cantHear = "I can't hear you!";
+        var yes = "YES INDEED!";
+        var love = "I would love to!";
+        if (string.toLowerCase(string) === string) {
+          return cantHear;
+        }
+        else if (string.toUpperCase(string) === string) {
+          return yes;
+        }
+        else if ("Let\'s have dinner together!" === string) {
+          return love;
+        }
+      }
